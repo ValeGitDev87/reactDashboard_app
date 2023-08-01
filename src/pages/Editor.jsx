dfba12e1269bf5cd07be13ca8f1d3fca0d950e4f
ValeGitDev87 <vale.sciarne@hotmail.com>
2023-08-01 20:52:15 +0200

    Update Editort components  with new changes

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
 
 import { Header } from '../components';
-import { EditorData } from '../data/dummy';
+
 
 const Editor = () => (
   <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-gradient-to-r from-blue-300 via-violet-700 to-pink-500 rounded-3xl">
